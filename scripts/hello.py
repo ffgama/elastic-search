5faba16f3a3a04b603ce863b2ba0d2385c1ba0ec
ffgama <f.fabiogama88@gmail.com>
2020-07-04 20:28:16 -0300

    preparing documents from elastic

--- scripts/hello.py
+++ scripts/hello.py
@@ -21,16 +21,28 @@ select_cities = dataset_cities.sample(n=20, replace=True, random_state=42).reset
 
 select_cities['person_id'] = random.randint(low=1, high=8, size=select_cities.shape[0])
 
-
 INDEX_NAME='cities_visited'
-# each row is a document
-for row in range(0, len(select_cities)):
-    document = dict( select_cities.apply(lambda x: x[row]) )
-    response[row] = es.index(INDEX_NAME, id=row, body=document)
-       
-# read
-es.get(index=INDEX_NAME, id=1)
-es.get(index=INDEX_NAME, id=19)
 
-# search
-# es.search(index=INDEX_NAME, body={'City':{'match_all':() }})
+if es.indices.exists(index=INDEX_NAME): es.indices.delete(index=INDEX_NAME)
+            
+# insert a data (document) into a INDEX         
+for row in range(0, len(select_cities)):    
+
+    document = dict(select_cities.apply(lambda x: x[row]))
+    results = es.index(INDEX_NAME, 
+                       id=row, 
+                       doc_type='cities', 
+                       body=document)
+
+results
+
+# show all documents that has been inserted
+all_docs = [es.get(index=INDEX_NAME, doc_type='cities', id=doc) for doc in range(0, len(select_cities))]
+all_docs
+
+# print only data
+[all_docs[item]['_source'] for item in range(0, len(all_docs) )]
+
+# became /cities_visited available
+# For example
+# http://localhost:9200/cities_visited/_search?q=City:Hollywood
