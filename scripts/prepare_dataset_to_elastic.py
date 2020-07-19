64dec2871499c96f7dd29bb50994639173424cd1
ffgama <f.fabiogama88@gmail.com>
2020-07-19 15:42:22 -0300

    utilities and elastic search methods

--- scripts/prepare_dataset_to_elastic.py
+++ scripts/prepare_dataset_to_elastic.py
@@ -2,8 +2,13 @@ from geopandas import read_file, GeoDataFrame
 from shapely.geometry import Point, Polygon
 from datetime import datetime
 from elasticsearch import Elasticsearch
-from pandas import read_csv, merge,DataFrame, concat
+from pandas import read_csv, DataFrame
 from numpy import random
+from pandas import set_option
+import json
+from utils.to_geojson import *
+
+set_option("display.max_rows", 500)
 
 # load shapefiles
 usa_states = read_file("data/cb_2018_us_state_500k.shp")
@@ -19,7 +24,6 @@ usa_states.columns = [col.lower() for col in usa_states.columns]
 
 # connect to elastic
 es = Elasticsearch()
-# list indexes
 es.indices.get_alias()
 
 INDEX_NAME="places_visited"
@@ -27,28 +31,21 @@ if es.indices.exists(index=INDEX_NAME): es.indices.delete(index=INDEX_NAME)
 
 # source: https://public.opendatasoft.com/explore/dataset/1000-largest-us-cities-by-population-with-geographic-coordinates/table/?sort=-rank
 dataset_state = read_csv("data/city_state_pop.csv", sep=";")
-#  dict(dataset_state["Coordinates"]).values()
 
-dataset_state["Coordinates"] = dataset_state["Coordinates"].apply(lambda m: m.split(","))
-
-for i, row in enumerate(dataset_state["Coordinates"]):
-    dataset_state["Coordinates"][i] = dict(lat = row[0], long=row[1])
+dataset_state[['latitude', 'longitude']] = \
+    dataset_state['Coordinates'].str.split(',', n=1, expand=True)
 
 dataset_state_sub = dataset_state.iloc[50:200:,]
 
-# select 
-dataset_state_sub = dataset_state_sub.sample(n=100, replace=True, random_state=42).reset_index()
+# random select 
+dataset_visited_state = dataset_state_sub.sample(n=100, replace=True, random_state=42).reset_index()
 
-dataset_state_sub["person_id"] = random.randint(low=1, high=8, size=dataset_state_sub.shape[0])
+dataset_visited_state["person_id"] = random.randint(low=1, high=8, size=dataset_visited_state.shape[0])
 
-dataset_state_sub.columns = [col.lower() for col in dataset_state_sub.columns]
+dataset_visited_state.columns = [col.lower() for col in dataset_visited_state.columns]
 
-dataset_state_sub.head()
-
-# combine datasets
-dataset_visited_state = dataset_state_sub
-
-columns_choosen = ["city","state","population","coordinates","person_id"]
+columns_choosen = ["city","state","population","coordinates",
+                   "person_id","latitude","longitude"]
 dataset_visited_state = dataset_visited_state[columns_choosen] 
 
 # create new columns
@@ -78,11 +75,56 @@ all_docs
 
 # became /cities_visited endpoint available
 # For example
-# http://localhost:9200/cities_visited/_search?q=City:Hollywood
+# http://localhost:9200/places_visited/_search?q=state:Texas
+
+# es.search(index=INDEX_NAME, body={"from":0, 
+# "size":0, 
+# "query":{"match": {"state":"Texas"} } })
+
+# case insensitve
+es.search(index=INDEX_NAME, 
+          body={"from":0, "size":0, "query":{"match": {"state":"TEXAS"} } })
+
+# change parameter "size": iterate over sentence whose state is "Texas"
+es.search(index=INDEX_NAME, 
+          body={ "from":0, "size":5, "query": {"match":{"state":"Texas"} } })
+
+# change parameter "from": define the offset from of query". In this case, we return only two last documents 
+es.search(index=INDEX_NAME, 
+          body={ "from":3, "size":5, "query": {"match":{"state":"Texas"} } })
+
+# match phrase prefix: get documents that contains words "park"
+# Cities: Lincoln Park and Oak Park
+es.search(index=INDEX_NAME, 
+          body={ "from":0, "size":3, "query": {"match_phrase_prefix":{"city":"Park"} }})
+
+# Combine multiple queries
+es.search(index=INDEX_NAME, 
+          body={ "from":0, "size":4, "query": {"bool": 
+                                        {
+                                        "must_not": {"match": {"city":"Texarkana"} },
+                                        "should": {"match": {"state":"Texas"} }
+                                        } } } )
+
+# Regex
+#  all states that begin with "i" letter
+# syntax regexp: https://www.elastic.co/guide/en/elasticsearch/reference/current/regexp-syntax.html
+es.search(index=INDEX_NAME, 
+          body={ "from":0, "size":17, "query": {"regexp": 
+                                    {"state":"i.*"}
+                                    } })
 
 df = DataFrame(all_docs)
 
 docs = [df.loc[:,"_source"][d] for d in range(0, len(df))]
 dataset_places = DataFrame.from_dict(docs)
 
+dataset_places['latitude'] = dataset_places['latitude'].astype(float)
+dataset_places['longitude'] = dataset_places['latitude'].astype(float)
 
+geojson = df_to_geojson(dataset_places, dataset_places.columns.to_list())
+
+with open('data/geo_places_visited.json', 'w') as json_file:
+    json.dump(geojson, json_file)
+    
+    
---
+++ scripts/utils/to_geojson.py
@@ -0,0 +1,18 @@
+def df_to_geojson(df, properties, lat='latitude', lon='longitude'):
+    
+    geojson = {'type':'FeatureCollection', 'features':[]}
+    
+    for _, row in df.iterrows():
+    
+        feature = {'type':'Feature',
+                   'properties':{},
+                   'geometry':{'type':'Point',
+                               'coordinates':[]}}
+        
+        feature['geometry']['coordinates'] = [row[lon], row[lat]]
+    
+        for prop in properties:
+            feature['properties'][prop] = row[prop]
+        geojson['features'].append(feature)
+    
+    return geojson
